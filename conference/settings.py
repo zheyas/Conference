de07eec1d0765ed8b9920910eb6b905c3d478cb3
Евгений <zhenyayasakov@yandex.ru>
2026-02-23 22:46:03 +0500

    add requirements.txt

--- conference/settings.py
+++ conference/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-58v15$*qkfxb7p=_egss!oio$f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '*').split(',')
 
 
 # Application definition
